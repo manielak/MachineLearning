f0b3f443a14bf492d32eef91f999a76c2f960cfe
manielak <mariusz.anielak@interia.pl>
2022-08-03 18:57:31 +0200

    numpy manipulations

--- PythonDataScience/numpy1.py
+++ PythonDataScience/numpy1.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+np.__version__
 
-L = list(range(11))
-print(L)
+test = np.array([1,4,2,5,3])
+test2 = np.array([3.14, 4, 2, 3], dtype=np.float32)
+test3 = np.array([range(i, i+3) for i in [2,4,6]])
+
+print(test3)
+
+
+# L = list(range(11))
+# print(L)
