1b1a1a920f0588c2ee89aaa5b31ac675e63cec28
manielak <mariusz.anielak@interia.pl>
2022-08-04 22:24:24 +0200

    Arrays manipulations

--- PythonDataScience/numpy1.py
+++ PythonDataScience/numpy1.py
@@ -1,12 +1,27 @@
 import numpy as np
 
-np.__version__
+scratch = np.zeros(10, dtype=int)
+scratch2 = np.zeros(10, dtype=np.bool_)
+my_ones = np.ones((3,5), dtype=float)
+my_full = np.full((3,5), 3.14)
+my_arrange = np.arange(0,30, 3)
+my_linespace = np.linspace(0,1, 5)
+my_random = np.random.random((5,7))
+my_normal_distribution = np.random.normal(0,1,(3,3))
+my_randint = np.random.randint(0,10,(3,3))
+my_eye = np.eye(3,3)
+my_empty = np.empty(4)
 
-test = np.array([1,4,2,5,3])
-test2 = np.array([3.14, 4, 2, 3], dtype=np.float32)
-test3 = np.array([range(i, i+3) for i in [2,4,6]])
 
-print(test3)
+# my_random = np.random.random((3,3))
+
+
+# test = np.array([1,4,2,5,3])
+# test2 = np.array([3.14, 4, 2, 3], dtype=np.float32)
+# test3 = np.array([range(i, i+3) for i in [2,4,6]])
+
+print(scratch2)
+
 
 
 # L = list(range(11))
