bea847e72e976687d9a2849a2fae450f5832ce30
manielak <mariusz.anielak@interia.pl>
2022-08-10 21:56:19 +0200

    Arrays manipulation.

--- PythonDataScience/arrays_manipulation.py
+++ PythonDataScience/arrays_manipulation.py
@@ -1,3 +1,10 @@
 import numpy as np
 
+rng = np.random.default_rng(seed=1701)  # seed for reproducibility
+
+x1 = rng.integers(10, size=6)  # One-dimensional array
+x2 = rng.integers(10, size=(3, 4))  # Two-dimensional array
+x3 = rng.integers(10, size=(3, 4, 5))  # Three-dimensional array
+
+print (x1)
 
